cc23a0b1f960d1d2d84b444ef50f3c92036d3acd
niuniu12341234 <165113339+niuniu12341234@users.noreply.github.com>
2024-04-03 17:04:10 +1300

    Beginner Output problem.py

---
+++ Beginner-s-Output-problem.py
@@ -0,0 +1,12 @@
+print ("Convert Minutes into Seconds")
+min = int(input("Input an integer in minutes.\n>>"))
+sec = min*60
+print ("It is",sec,"seconds.")
+print ("Jane always gets at least twice as many birthday cards as her older sister Cruella.")
+Cruella = int(input("How many cards Cruella received for her last birthday?\n>>"))
+Jane = Cruella*2
+print ("Jane receives at least",Jane,"cards.")
+print ("Age to Days")
+age = int(input("Enter your age in years.\n>>"))
+day = age*365
+print("You are",day,"days old.")
